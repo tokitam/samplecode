d1d3d06cb07a4b9a6462c1c8ec6520ad2458b1a4
masahiko tokita <tokita-masahiko@cyber-z.co.jp>
2018-09-07 15:43:13 +0900

    min, max setting

--- c/prime_number/prime_number.c
+++ c/prime_number/prime_number.c
@@ -25,20 +25,25 @@ int check_prime_number(int number)
 
 int main(int argc, char *argv[])
 {
-    int i, number;
-   
+    int i, min, max;
+
     if (argc < 2) {
         usage();
         return 0;
+    } else if (argc == 2) {
+        min = 2;
+        max = atoi(argv[1]);
+    } else {
+        min = atoi(argv[1]);
+        max = atoi(argv[2]);
     }
-   
-    number = atoi(argv[1]);
 
-    for (i=2; i <= number; i++) {
+
+    for (i=min; i <= max; i++) {
         if (check_prime_number(i)) {
             printf("%d\n", i);
         }
     }
-   
+
     return 0;
 }
